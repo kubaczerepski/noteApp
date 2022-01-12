170f998f25b45c93a48caede1c8c9e1f7fb433e6
Theerapy <kubaczerepski@gmail.com>
2022-01-12 21:01:48 +0100

    Welcome Page

--- welcomePage.php
+++ welcomePage.php
@@ -7,9 +7,28 @@
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel='stylesheet' type='text/css' media='screen' href='./CSS/mainStyles.css'>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mukta:wght@600&display=swap" rel="stylesheet">
 </head>
 <body>
-    
+    <section class="container-fluid d-flex justify-content-center align-items-center" id="welcomeContainer">
+        <h1 id="welcomeHeader">Welcome</h1>
+    </section>
+    <section class="container-fluid d-flex justify-content-center align-items-center" id="formContainer">
+        <form method="POST" class="form-floating" id="loginForm">
+            <div class="form-floating mb-3">
+                    <input type="email" class="form-control" id="floatingInput" placeholder="Email">
+                    <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+            </div>
+            <button type="submit" class="btn btn-primary btn-block mb-4">Login</button>
+        </form>
+        <h5>Don't have an account? <a href="./php/registerPage.php">Join us!</a></h2>
+    </section>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
